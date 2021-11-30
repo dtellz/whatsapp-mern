ea3746c6c14d65895ea17518dba2cd55b40b4d0c
dtellz <dtellez95@gmail.com>
2021-12-01 00:56:01 +0100

    basic structure stablished

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,24 +1,48 @@
 import React from 'react';
 import './Sidebar.css'
 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
+
+import SidebarChat from './SidebarChat';
+
+
+
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>I am a sidebar</h1>
             <div className='sidebar__header'>
-
+                {/**TODO bring images from user collection */}
+                <Avatar src='https://static3.depositphotos.com/1000958/117/i/600/depositphotos_1178129-stock-photo-kitten.jpg' />
                 <div className='sidebar__headerRight'>
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className='sidebar__search'>
+                <div className='sidebar__searchContainer'>
+                    <SearchOutlined />
+                    <input placeholder='Search or start a new chat' type='text' />
+
 
                 </div>
-
-
             </div>
 
-            <div className='sidebar__'>
+            <div className='sidebar__chats'>
 
-            </div>
-
-            <div className='sidebar__'>
+                <SidebarChat />
+                <SidebarChat />
+                <SidebarChat />
 
             </div>
         </div>
