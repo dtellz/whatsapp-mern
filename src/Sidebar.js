51f14bf568961727ce803c3c5595ac7d8b431274
dtellz <dtellez95@gmail.com>
2021-11-30 23:00:54 +0100

    main view styling

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Sidebar.css'
 
-function Sidebar(){
-    return(
+function Sidebar() {
+    return (
         <div className='sidebar'>
             <h1>I am a sidebar</h1>
+            <div className='sidebar__header'>
+
+                <div className='sidebar__headerRight'>
+
+                </div>
+
+
+            </div>
+
+            <div className='sidebar__'>
+
+            </div>
+
+            <div className='sidebar__'>
+
+            </div>
         </div>
     )
 }
